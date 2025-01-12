62a381b22add2d51dd3f18fdfed5c73786d0366a
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2025-01-12 17:53:53 +0530

    fix: update ref type in SidebarGroup and upgrade @types/react to 18.3.12

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -419,7 +419,7 @@ const SidebarGroup = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <div
-      ref={ref as React.Ref<HTMLDivElement>}
+      ref={ref}
       data-sidebar="group"
       className={cn("relative flex w-full min-w-0 flex-col p-2", className)}
       {...props}
